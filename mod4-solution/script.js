0c8c23578e89ee6da4b75803c046dea3634dd942
unknown <suvabvinaik13@outlook.com>
2020-07-11 17:18:14 +0530

    mod 4 solution

---
+++ mod4-solution/script.js
@@ -0,0 +1,18 @@
+
+(function (){
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for(var i = 0; i < names.length; i++){
+  
+  var firstletter = names[i].charAt(0);
+    var firstlower = firstletter.toLowerCase();
+
+  if (firstlower == 'j') {
+    byeSpeaker.speak(names[i]);
+  } 
+
+  else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
